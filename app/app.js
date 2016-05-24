17f21018f573917ef9559bafa44a54d5244901b8
ShermanDiep <mhung8486@gmail.com>
2016-05-24 19:16:02 +0700

    update source and layout

--- app/app.js
+++ app/app.js
@@ -13,13 +13,25 @@ app.controller('mycontroller', function($scope, $timeout) {
 				$scope.article = "2";
 				break;
 			case "2":
+				$scope.article = "3";
+				break;	
+			case "3":
+				$scope.article = "4";
+				break;	
+			case "4":
+				$scope.article = "5";
+				break;	
+			case "5":
+				$scope.article = "6";
+				break;	
+			case "6":
 				$scope.article = "1";
 				break;			
 			default:
 				$scope.article = "1";
 			}
 		}
-		$timeout(doSomething, 4500);	  	  
+		$timeout(doSomething, 3000);	  	  
 	}
-	$timeout(doSomething, 4500);
+	$timeout(doSomething, 3000);
 });
