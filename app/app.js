b7abe9de8fb98048faf54b0d2e7a164701e62236
ShermanDiep <mhung8486@gmail.com>
2016-05-24 11:41:17 +0700

    fix error and update animation v2

--- app/app.js
+++ app/app.js
@@ -2,3 +2,24 @@
 
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', ['ngAnimate']);
+app.controller('mycontroller', function($scope, $timeout) {
+	$scope.article = '1';
+	$scope.time = '400';
+	console.log("aaaaaaa");
+	function doSomething() {
+		if(true){
+			switch($scope.article){
+			case "1":
+				$scope.article = "2";
+				break;
+			case "2":
+				$scope.article = "1";
+				break;			
+			default:
+				$scope.article = "1";
+			}
+		}
+		$timeout(doSomething, 4500);	  	  
+	}
+	$timeout(doSomething, 4500);
+});
